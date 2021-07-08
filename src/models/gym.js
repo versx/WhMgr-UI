6e48e184c1d1711f17cc22bd99fed5f718dbd712
versx <versx@ver.sx>
2021-07-07 17:00:29 -0700

    >.>

--- src/models/gym.js
+++ src/models/gym.js
@@ -2,6 +2,7 @@
 
 const { DataTypes, Model, } = require('sequelize');
 const sequelize = require('../services/sequelize.js')(true);
+const { parseJsonColumn } = require('../services/utils.js');
 
 class Gym extends Model {
 
